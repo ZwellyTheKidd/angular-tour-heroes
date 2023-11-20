a68968ffbfac4a7f38e00620c2240a4afa35fea9
ZwellyTheKidd <zwelly.thekidd@outlook.com>
2023-11-20 11:19:34 +0200

    Initial commit

---
+++ src/app/background/background.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-background',
+  templateUrl: './background.component.html',
+  styleUrls: ['./background.component.scss']
+})
+export class BackgroundComponent {
+
+  isRed = false;
+
+
+change() {
+  this.isRed = !this.isRed;
+}
+
+
+}
