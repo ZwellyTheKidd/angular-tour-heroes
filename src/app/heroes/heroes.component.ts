f40defa1eb2f0c0203c63dd40750cee7a77a060f
ZwellyTheKidd <zwelly.thekidd@outlook.com>
2023-11-21 09:41:43 +0200

    heroes

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,13 +1,18 @@
 import { Component } from '@angular/core';
 import {HEROES} from '../mock-heroes';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.scss'],
-  
 
+  standalone: true,
+  imports: [
+    NgFor,
+  ],
 })
+
 export class HeroesComponent {
   heroes = HEROES;
 }
